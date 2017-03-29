53668725e7a1ab1d004b5824f3d3c95728408119
ΔNDRΞW <drewdrummer88@gmail.com>
2017-03-28 20:42:12 -0500

    built out controller and views for chat rooms.

---
+++ app/controllers/chat_rooms_controller.rb
@@ -0,0 +1,26 @@
+class ChatRoomsController < ApplicationController\
+  def index
+    @chat_rooms = ChatRoom.all
+  end
+
+  def new
+    @chat_room = ChatRoom.new
+  end
+
+  def create
+    @chat_room = current_user.chat_rooms.build(chat_room_params)
+    if @chat_room.save
+      flash[:success] = 'Chat room added! ^_^'
+      redirect_to chat_rooms_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+
+  def chat_rooms_params
+    params.require(:chat_room).permit(:title)
+  end
+
+end
