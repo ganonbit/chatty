67e1dcc04432836073f35c276f675c3999adc10f
ΔNDRΞW <drewdrummer88@gmail.com>
2017-04-27 17:02:26 -0500

    fixed routes after upgrade. broken till i can turn on devise

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,10 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  devise_for :users
+  
+  resources :chat_rooms, only: [:new, :create, :show, :index]
+  
+  mount ActionCable.server => '/cable'
+  
+  root 'chat_rooms#index'
+
 end
