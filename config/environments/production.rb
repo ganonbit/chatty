c2e9ebc4d1998d03c7433eee3b1e49605deead04
ΔNDRΞW <drewdrummer88@gmail.com>
2017-04-27 16:56:35 -0500

    ran rails app:update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,9 +41,9 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  config.action_cable.url = "wss://chatty5.herokuapp.com/cable"
-  config.action_cable.allowed_request_origins = ['https://chatty5.herokuapp.com','http://chatty5.herokuapp.com']
-  
+  # config.action_cable.url = 'wss://example.com/cable'
+  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
 
--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,3 @@
 Rails.application.routes.draw do
-  devise_for :users
-  
-  resources :chat_rooms, only: [:new, :create, :show, :index]
-  
-  mount ActionCable.server => '/cable'
-  
-  root 'chat_rooms#index'
-
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
